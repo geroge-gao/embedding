c879aeaf2cf128a86633697fb4153352c09f5408
geroge-gao <1375641052@qq.com>
2021-12-26 15:35:47 +0800

    update: add bert folder

--- base_embedding/transformer/transformer.py
+++ base_embedding/transformer/transformer.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import tensorflow as tf
-import torch as t 
+"""
 
-class transformer(object):
+"""
+ 
+import pandas as pd
+import tensorflow as tf
+
+from tensorflow.keras.initializers import GlorotNormal
+from tensorflow.keras.layers import Layer
+
+
+
+class transformer(Layer):
+
+    def __init__(self, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
+        super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
+
