498a4480c7ecdbd7f70f235f73e9fc587c6b50ce
geroge-gao <1375641052@qq.com>
2022-01-11 00:42:48 +0800

    test mask

--- base_embedding/transformer/layers.py
+++ base_embedding/transformer/layers.py
@@ -28,22 +28,31 @@ class FFNLayer(Layer):
 
 class EncoderLayer(Layer):
 
-    def __init__(self, d_model, num_heads, dff, mask, rate=0.1, ):
+    def __init__(self, d_model, num_heads, dff, mask_zero, rate=0.1, ):
         super(EncoderLayer, self).__init__()
         self.d_model = d_model
         self.num_heads = num_heads
         self.dff = dff
         self.rate = rate
+        self.mask = mask_zero
         self.attentions = []
         for i in range(num_heads):
-            self_attention = Self_Attention(self.d_model, name="head_{}".format(i), mask=mask)
+            self_attention = Self_Attention(self.d_model, name="head_{}".format(i), mask=self.mask)
             self.attentions.append(self_attention)
 
-        
+        self.dropout1 = Dropout(rate)
+        self.dropout2 = Dropout(rate)
 
     def call(self, inputs, mask):
-        # return super().call(inputs, *args, **kwargs)
-        
+        pass
+
+    
+    def compute_mask(self, inputs, mask=None):
+        if self.mask:
+            return tf.not_equal(inputs, 0)
+        else:
+            return None 
+
 
 class DecoderLayer(Layer):
 
